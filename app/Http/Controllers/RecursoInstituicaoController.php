233ec195348746da5f6427d3495fd9b5b804d538
Your Name <you@example.com>
2022-01-24 19:49:51 -0300

    criado controller

---
+++ app/Http/Controllers/RecursoInstituicaoController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RecursoInstituicao;
+use Illuminate\Http\Request;
+
+class RecursoInstituicaoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\RecursoInstituicao  $recursoInstituicao
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RecursoInstituicao $recursoInstituicao)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\RecursoInstituicao  $recursoInstituicao
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RecursoInstituicao $recursoInstituicao)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\RecursoInstituicao  $recursoInstituicao
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RecursoInstituicao $recursoInstituicao)
+    {
+        //
+    }
+}
